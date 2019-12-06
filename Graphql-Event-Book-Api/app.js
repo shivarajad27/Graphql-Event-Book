6c9e4ca2208ddfee19ecd6df2aff310599153616
shivaraja_vince <shivaraja@vince.no>
2019-12-06 18:47:45 +0530

    Database setup

--- Graphql-Event-Book-Api/app.js
+++ Graphql-Event-Book-Api/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const port = process.env.PORT || 3000;
 const app = express()
@@ -45,15 +47,29 @@ const rootValue = {
         return events;
     },
     createEvent: (args) =>{
-        const event = {
-            _id:Math.random().toString(),
+        // const event = {
+        //     _id:Math.random().toString(),
+        //     title:args.eventInput.title,
+        //     description:args.eventInput.description,
+        //     price:+args.eventInput.price,
+        //     date:new Date().toString()
+        // }
+
+        const event = new Event({
             title:args.eventInput.title,
             description:args.eventInput.description,
             price:+args.eventInput.price,
             date:new Date().toString()
-        }
-        events.push(event);
-        return event;
+        })
+        
+        return event.save()
+        .then(result=>{
+            return {...result._doc};
+        })
+        .catch(err=>{
+            console.log(err);
+            throw err;
+        })
     }
 }
 
@@ -63,4 +79,11 @@ app.use('/graphql',graphqlHttp({
     graphiql:true
 }));
 
-app.listen(port,()=>console.log('server started'));
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@devmeetups-uqzdb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{ useNewUrlParser: true })
+.then(()=>{
+    console.log("DB connected");
+    app.listen(port,()=>console.log('server started'));
+})
+.catch(err=>{
+    console.log(err);
+})
